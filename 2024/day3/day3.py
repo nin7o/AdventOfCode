9254b939907962d2dd0813d6107bbb21e94a29e4
Nin7o <nin7o.g@proton.me>
2024-12-03 06:34:33 +0100

    oups revoila la p1

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -8,6 +8,7 @@ pattern_dont = r"don\'t\(\)"
 combined_pattern = f"{pattern}|{pattern_do}|{pattern_dont}"
 data = ""
 total = 0
+total_1 = 0
 do = True
 
 with open ('input', 'r') as f:
@@ -18,14 +19,12 @@ with open ('input', 'r') as f:
         for match in re.finditer(combined_pattern, data):
             print(match.group())
             if re.search(pattern, match.group()):
+                total_1 += int(match.group(1)) * int(match.group(2))
                 total += int(match.group(1)) * int(match.group(2)) if do else 0
             elif re.search(pattern_do, match.group()):
                 do = True
             elif re.search(pattern_dont, match.group()):
                 do = False
         
-
-        
-        
-
-print(total)
+print("p1 :", total_1)
+print("p2 :", total)
