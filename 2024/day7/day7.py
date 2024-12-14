14f0c765f728e4ee7637dc4be5ab833e754fb6c0
nin7o <nin7o.g@proton.me>
2024-12-14 21:27:15 +0100

    day7!

--- 2024/day7/day7.py
+++ 2024/day7/day7.py
@@ -4,60 +4,40 @@ import pprint
 valid = 0
 valid2 = 0
 
+#q,r = divmod(10, 3)
+
 def is_valid(res, num_list):
     last = num_list.pop()
-    #print(f"res = {res}, last = {last}, num_list = {num_list}")
     if len(num_list) == 0:
-        if last == res:
-            print(f"res == last : {res} == {last} -> True")
-            return True
-        else:
-            #print(f"res != last : {res} != {last} -> False")
-            return False
+        return last == res
     if res % last == 0:
-        #print(f"on tente la division : {res} / {last}")
         if is_valid(res//last, num_list.copy()):
             return True
-        else:
-            #print(f"la div a pas marchée : {res} / {last}, substracting {last} from {res} = {res-last}")
-            return is_valid(res-last, num_list.copy())
-    else: 
-        #print(f"on tente la soustraction : substracting {last} from {res} = {res-last}")
+    if res - last >= 0: 
         return is_valid(res-last, num_list.copy())
-    
+    else:
+        return False
+  
 def is_valid_p2(res, num_list):
     last = num_list.pop()
-    #print(f"res = {res}, last = {last}, num_list = {num_list}")
+    print(f"res = {res}, last = {last}, num_list = {num_list}")
     if len(num_list) == 0:
-        if last == res:
-            #print(f"res == last : {res} == {last} -> True")
-            return True
-        else:
-            #print(f"res != last : {res} != {last} -> False")
-            return False
+        return last == res
     if res % last == 0:
-        #print(f"on tente la division : {res} / {last}")
+        print(f"on tente la division : {res} / {last}")
         if is_valid_p2(res//last, num_list.copy()):
             return True
-        else:
-            #print(f"la div a pas marchée : {res} / {last}, substracting {last} from {res} = {res-last}")
-            if is_valid_p2(res-last, num_list.copy()):
-                return True
-            else:
-                #print(f"la soustraction a pas marchée : {res} - {last} -> on tente la concaténation : {res}{last}")
-                new_list = num_list.copy()
-                new_list[-1] = int(str(new_list[-1]) + str(last))
-                #print(f"new_list = {new_list}")
-                return is_valid_p2(res, new_list)
-    else: 
-        #print(f"on tente la soustraction : substracting {last} from {res} = {res-last}")
+    if res - last >= 0: 
+        print(f"on tente la soustraction : substracting {last} from {res} = {res-last}")
         if is_valid_p2(res-last, num_list.copy()):
             return True
-        else:
-            #print(f"la soustraction a pas marchée : {res} - {last} -> on tente la concaténation : {res}{last}")
-            new_list = num_list.copy()
-            new_list[-1] = int(str(new_list[-1]) + str(last))
-            return is_valid_p2(res, new_list)
+    res_str = str(res)
+    last_str = str(last)
+    if res_str.endswith(last_str):
+        new_res = int(res_str[:-len(last_str)])
+        if is_valid_p2(new_res, num_list.copy()):
+            return True
+    
             
 
 with open ('input', 'r') as f:
@@ -66,13 +46,14 @@ with open ('input', 'r') as f:
         line2 = line.strip().split(':')
         res = int(line2[0])
         operands = [int(x) for x in line2[1].split()]
-        #print("====================================")
-        #print(operands)
-        if is_valid(copy.deepcopy(res), operands):
+        if is_valid(copy.deepcopy(res), copy.deepcopy(operands)):
             valid += res
             valid2 += res
-        elif is_valid_p2(copy.deepcopy(res), operands):
+        elif is_valid_p2(copy.deepcopy(res), copy.deepcopy(operands)):
             valid2 += res
+            print(f"TRUE -> {res}")
+        else:
+            print(f"FALSE -> {res}")
   
 print("Part 1 : ", valid)
 print("Part 2 : ", valid2) 
